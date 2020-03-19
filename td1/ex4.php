a836bcc095284e052af42f5c274319fd59630f86
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-03-19 18:40:33 +0100

    Update ex4.php

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,13 +1,8 @@
-<table border='1'>
-    <tbody>
-        <tr>
-            <td>1-1</td>
-            <td>1-2</td>
-        </tr>
-        <tr>
-            <td>2-1</td>
-            <td>2-2</td>
-        </tr>
-    </tbody>
-</table>
+<h1>ex4</h1>
+<a href="?message">tableau XS</a><br>
+<a href="?message">tableau S</a><br>
+<a href="?message">tableau M</a>
+
+
+
 
