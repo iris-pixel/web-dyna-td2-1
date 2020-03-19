3d87518a2e5b262551e5fbe8b9e79eaa4fc975d5
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-03-19 17:07:05 +0100

    Update ex1.php

--- td1/ex1.php
+++ td1/ex1.php
@@ -1,3 +1,7 @@
-<?php
-echo 'Hello world'; 
-?>
+<h1>ex1</h1>
+<a href="?message=tp1">a</a><br>
+<a href="?message=tp2">b</a><br>
+<a href="?message=tp3">c</a>
+<h1>
+	<?=$_GET['message']??'pas de message' ?>
+</h1>
