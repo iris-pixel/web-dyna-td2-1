d62b4249edeb597ea348a45b10bb5bb0eb33ed6b
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-03-25 08:47:47 +0100

    Update ex4.php

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,8 +1,27 @@
-<h1>ex4</h1>
 <a href="?message">tableau XS</a><br>
 <a href="?message">tableau S</a><br>
 <a href="?message">tableau M</a>
 
+<table border='1'>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+        </tr>
+        <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+        </tr>
+    </tbody>
+</table>
 
+<?php
+$size=$_GET["size"]??"XS";
+?>
+<?php
+$size=$_GET["size"]??"XS";
+?>
+<div style="font-size"> Tableau:
+</div>
 
 
