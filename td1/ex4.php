564e7216955ebee21e4ce77e2edd94a4088a4023
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-03-25 09:22:20 +0100

    Update ex4.php

--- td1/ex4.php
+++ td1/ex4.php
@@ -2,27 +2,3 @@
 <a href="?tableau">tableau S</a><br>
 <a href="?tableau">tableau M</a>
 
-<html>
-<body>
-<?php
-// $NbrCol : le nombre de colonnes
-// $NbrLigne : le nombre de lignes
-// -------------------------------------------------------
-echo '<table>';
-for ($i=1; $i<=$NbrLigne; $i++) {
-   echo '<tr>';
-   for ($j=1; $j<=$NbrCol; $j++) {
-         echo '<td>';
-          // ------------------------------------------
-          // AFFICHAGE ligne $i, colonne $j
-         echo $affichage;
-          // ------------------------------------------
-         echo '</td>';
-   }
-   echo '</tr>';
-   $j=1;
-}
-echo '</table>';
-?>
-</body>
-</html> 
