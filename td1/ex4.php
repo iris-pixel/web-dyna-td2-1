bc43f7bc1d6cba56290f95535480cfa662b226af
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-03-25 10:01:03 +0100

    Update ex4.php

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,7 +1,16 @@
-<?php $title='exercice 4';
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+<hr>
+<?php 
+$title='exercice 4 (Itérations)';
 include "include/header.php";
+$nbLignes=$_GET["l"]??10;
+$nbColonnes=$_GET["c"]??10;
+echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
 ?>
 
 <?php
 include "include/footer.php";
 ?>
+
