07af3b4ce3892622140ecfe195cec7cf757f3df1
iris-pixel <62330763+iris-pixel@users.noreply.github.com>
2020-04-02 14:49:42 +0200

    Update ex4.php

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,31 +1,33 @@
+<?php
+$title='Exercice n°4 (Itérations)';
+include "include/header.php";
+?>
 <a href="?l=2&c=2">Tableau 2 x 2</a><br>
 <a href="?l=5&c=5">Tableau 5 x 5</a><br>
 <a href="?l=10&c=8">Tableau 10 x 8</a><br>
 <hr>
-<?php 
-$title='exercice 4 (Itérations)';
-include "include/header.php";
+<?php
+//Récupère les paramètres l et c passés dans l'url
 $nbLignes=$_GET["l"]??10;
 $nbColonnes=$_GET["c"]??10;
 echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
 ?>
-
 <table border='1'>
-	<tbody>
-		<?php for($i=0;$i<$nbLignes;$i++){?>
-		<?php for($i=0;$i<$nbLignes;$i++){
-			$gras=($i%2==0)?"bold":"normal";
-		?>
-			<tr>
-			<?php for($j=0;$j<$nbColonnes;j++){?>
-				<td><?="$i - $j"?></td>
-				<td style="font-weight: <?=$gras ?>"<?="$i - $j"?></td>
-			<?php }?>
-			</tr>
-		<?php }?>
-	</tbody>
-</table> 
+    <tbody>
+      <?php for($i=0;$i<$nbLignes;$i++){
+        $gras=($i%2==0)?"bold":"normal";
+      ?>
+        <tr>
+        <?php for($j=0;$j<$nbColonnes;$j++){
+            $couleur=($j%2==0)?"red":"black";
+            ?>
+            <td style="font-weight: <?=$gras ?>;color:<?=$couleur ?>"><?="$i - $j"?></td>
+        <?php }?>
+        </tr>
+      <?php }?>
+    </tbody>
+</table>
 <?php
 include "include/footer.php";
-?>
+ ?>
 
